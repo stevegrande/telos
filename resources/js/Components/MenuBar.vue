ad7d67277449a42fc130e526ed04802ebc1e7c14
stevegrande <steve@terradigm.ca>
2024-03-22 19:49:31 -0500

    More files

---
+++ resources/js/Components/MenuBar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center border-blue-200 mt-4 text-3xl pt-2 text-white font-bold text-center">
+        <span>THE FOUNDATION:  <slot  /></span>
+    </div>
+</template>
---
+++ resources/js/Components/MenuLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+const props = defineProps(['to', 'label']);
+
+</script>
+<template>
+    <li class = "ml-4 italic cursor-pointer hover:text-blue-900">
+        <Link :href="props.to">
+            <span>{{props.label}}</span>
+        </Link>
+    </li>
+</template>
